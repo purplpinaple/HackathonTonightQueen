abb00e9409b303b1aabf75c7dd6d175b18b74f7a
purplpinaple <haugheyfelim@gmail.com>
2026-03-16 16:42:23 +0000

    update server filesX

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const PORT = 3000
+
+app.get('/',( req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.get('/video',(req,res) =>{
+		const videoPath = path.join(__dirname, 'sample.mp4')
+		const videoSize = fs.statSync(videoPath).size
+
+		const range = req.headers.range
+		if(!range) {
+			res.status(400).send("Requires Range Header")
+		}
+	const CHUNK_SIZE = 10 **6
+		
+	const start = Number(range.replace(/\D/g,""))
+	const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+	const headers = {
+		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+		"Accept-Ranges": "bytes",
+		"Content-Length": end-start + 1,
+		"Content-Type": "video/mp4"
+	}
+
+	res.writeHead(206, headers)
+
+	const videoStream = fs.createReadStream(videoPath, {start, end})
+	videoStream.pipe(res)
+})
+app.listen(PORT,() => {
+	console.log(`Server running on port ${PORT}`)
+})
